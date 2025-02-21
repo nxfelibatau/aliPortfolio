eb4c8fbe2b77021dafdc77440db97c5827b5d6e8
yasi <auroramarple@gmail.com>
2025-02-21 11:09:40 +0330

    second page done

--- app/components/BackgroundBeams.jsx
+++ app/components/BackgroundBeams.jsx
@@ -1,17 +1,21 @@
 "use client";
 import React from "react";
 import { BackgroundBeams } from "./ui/background-beams";
+import StickyScrollReveal from "./StickyScrollReveal";
 
 export default function BackgroundBeamsDemo() {
   return (
-    (<div
-      className="h-[40rem] w-full rounded-md bg-navy relative flex flex-col items-center justify-center antialiased">
-      <div className="max-w-2xl mx-auto p-4">
-        <h1
-          className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-        </h1>
+    <div className="relative h-[100rem] w-full rounded-md bg-navy flex flex-col items-center justify-center antialiased">
+      {/* پس‌زمینه باید پایین‌ترین لایه باشد */}
+      <div className="absolute inset-0 z-0">
+        <BackgroundBeams />
       </div>
-      <BackgroundBeams />
-    </div>)
+
+
+      {/* کامپوننت StickyScrollReveal */}
+      <div className="relative z-10 w-full">
+        <StickyScrollReveal />
+      </div>
+    </div>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -4,17 +4,19 @@ import React from "react";
 import { Cover } from "./components/ui/Cover";
 import BackgroundBeams from "./components/BackgroundBeams";
 import FlipWords from "./components/FlipWords";
+import StickyScrollReveal from "./components/StickyScrollReveal";
 
 export default function Home() {
   return (
     <>
       <div className="mt-16">
         <ParticlesComponent />
-        {/* firstpage */}
-        <div style={{height:"67vh"}}>
-          <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-52 relative z-1 py-6 bg-clip-text text-transparent bg-gradient-to-b from-[#ACF0D1] via-[#ACF0D1] to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+        
+        {/* صفحه اول */}
+        <div style={{ height: "67vh" }}>
+          <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-52 relative z-10 py-6 bg-clip-text text-transparent bg-gradient-to-b from-[#ACF0D1] via-[#ACF0D1] to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
             Welcome to
-            <br />{" "}
+            <br />
             <div className="mt-3">
               <Cover>My portfolio</Cover>
             </div>
@@ -23,9 +25,13 @@ export default function Home() {
             <FlipWords />
           </div>
         </div>
-        {/* second page */}
-        <div className="">
-          <BackgroundBeams />
+
+        {/* صفحه دوم */}
+        <div className="relative ">
+          {/* پس‌زمینه صفحه دوم */}
+          <div className="absolute inset-0 z-0">
+            <BackgroundBeams />
+          </div>
         </div>
       </div>
     </>
